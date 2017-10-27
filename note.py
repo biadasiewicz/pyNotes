c738cca58cd5c9562de8a57ac0e6ebe9992a4ce6
tomek <biadasiewicz@protonmail.com>
2017-10-27 14:35:04 +0200

    inserting and selecting note to database

--- note.py
+++ note.py
@@ -1,5 +1,4 @@
 import time, re
-import sqlite3
 
 class note:
     def __init__(self, text, timestamp = time.time()):
@@ -10,14 +9,21 @@ class note:
         self.text = text
     def is_tagged(self, tag):
         return tag in self.tags
-    def __conform__(self, protocol):
-        if protocol is sqlite3.PrepareProtocol:
-            return "%f;%s" % (self.timestamp, self.text)
+    def __eq__(self, other):
+        import math
+        return math.isclose(self.timestamp, other.timestamp) and self.text == other.text
+    def __str__(self):
+        return "%f: %s" % (self.timestamp, self.text)
+    def __repr__(self):
+        return "(%f;%s)" % (self.timestamp, self.text)
+def adapt_note(n):
+    return ("%f;%s" % (n.timestamp, n.text)).encode('ascii')
 def convert_note(s):
-    m = list(map(str, s.split(b";")))
-    assert(len(m) == 2)
-    n = note(float(m[0]))
-    n.set_text(m[1])
+    loaded = list(map(str, s.split(b";")))
+    assert(len(loaded) == 2)
+    ts = float(loaded[0][2:len(loaded[0]) - 1])
+    t = loaded[1][2:len(loaded[1]) - 1]
+    n = note(t, ts)
     return n
 
 n = note("hello #tag1 #tag1 #tag2 #tag_3")
@@ -26,3 +32,15 @@ assert(n.is_tagged('tag2'))
 assert(n.is_tagged('tag_3'))
 
 
+import sqlite3
+sqlite3.register_adapter(note, adapt_note)
+sqlite3.register_converter("note", convert_note)
+con = sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES)
+cur = con.cursor()
+cur.execute("create table test(n note)")
+cur.execute("insert into test(n) values (?)", (n,))
+cur.execute("select n from test")
+n2 = cur.fetchone()[0]
+assert(n == n2)
+cur.close()
+con.close()
