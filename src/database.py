e4655bf6021f5dfb15b4143893fff4990d29dbba
tomek <biadasiewicz@protonmail.com>
2017-11-01 17:24:12 +0100

    database initialization changed

--- src/database.py
+++ src/database.py
@@ -2,9 +2,19 @@ import note, sqlite3
 
 class Database:
     def __init__(self, path):
-        self.con = sqlite3.connect(path, detect_types=sqlite3.PARSE_DECLTYPES)
+        self.con = sqlite3.connect(path)
         self.cur = self.con.cursor()
-        self.cur.execute("create table if not exists notes(n Note)")
+        self.cur.execute("pragma foreign_keys = ON")
+        self.cur.execute("""create table if not exists
+            notes(
+            noteid integer primary key autoincrement,
+            notets timestamp not null,
+            notetext text not null)""")
+        self.cur.execute("""create table if not exists
+            tags(
+            tagid integer,
+            tagtext text not null,
+            FOREIGN KEY(tagid) references notes(noteid))""")
 
     def close(self):
         self.cur.close()
