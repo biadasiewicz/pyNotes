b000a1dddba981ac8d27dd86f7724a49c6ce6b83
tomek <biadasiewicz@protonmail.com>
2017-11-02 15:40:40 +0100

    formating fixes

--- src/database.py
+++ src/database.py
@@ -15,6 +15,7 @@ class Database:
             tagid integer,
             tagtext text not null,
             FOREIGN KEY(tagid) references notes(noteid))""")
+
     def insert_note(self, n):
         self.cur.execute("insert into notes(notets, notetext) values(?, ?)",\
             (n.timestamp, n.text))
@@ -22,20 +23,25 @@ class Database:
         for tag in n.tags:
             self.cur.execute("insert into tags(tagid, tagtext) values(?, ?)",\
                 (id, tag))
+
     def delete_note(self, id):
         self.cur.execute("delete from tags where tagid=?", (id,))
         self.cur.execute("delete from notes where noteid=?", (id,))
+
     def select_date_range(self, a, b):
         self.cur.execute("""select noteid, notets notetext from notes
             where notets >= (?) and notets <= (?)
             order by notets asc""", (a, b,))
         return self.cur.fetchall()
+
     def count(self):
         self.cur.execute("select noteid from notes")
         return len(self.cur.fetchall())
+
     def max_id(self):
         self.cur.execute("select max(noteid) from notes")
         return self.cur.fetchone()[0]
+
     def close(self):
         self.cur.close()
         self.con.close()
--- src/note.py
+++ src/note.py
@@ -4,15 +4,20 @@ class Note:
     def __init__(self, text, timestamp = datetime.datetime.now()):
         self.timestamp = timestamp
         self.set_text(text)
+
     def set_text(self, text):
         self.tags = set(re.findall(r"#(\w+)", text))
         self.text = text
+
     def is_tagged(self, tag):
         return tag in self.tags
+
     def __eq__(self, other):
         import math
         return math.isclose(self.timestamp, other.timestamp) and self.text == other.text
+
     def __str__(self):
         return "%f: %s" % (self.timestamp, self.text)
+
     def __repr__(self):
         return "(%f;%s)" % (self.timestamp, self.text)
